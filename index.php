884de6d83f3e8bdd87e6e9dd6cea09f83576b0c4
Federico Pirani <freonius@gmail.com>
2019-11-18 11:47:30 +0100

    Jumbotron pictures

--- index.php
+++ index.php
@@ -255,7 +255,7 @@ else {
     $array["robots"] = "follow,index,noodp";
 }
 $christmas = false;
-$bcks = array("bck1", "bck2", "bck3", "bck4", "bck5", "bck6", "bck7", "bck8");
+$bcks = array("bck1", "bck2", "bck4", "bck5", "bck6", "bck7", "bck10");
 if ($christmas) {
     $bcks = array("bck10");
 }
